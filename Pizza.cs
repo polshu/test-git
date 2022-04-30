838eb5426266d815f66319bac6cdfe4955702c5b
Pablo Ulman <polshu@polshu.com.ar>
2022-04-30 19:20:12 -0300

    Primer commit

---
+++ Pizza.cs
@@ -0,0 +1,7 @@
+public class Pizza {
+    public int      Id              { get; set; }
+    public string   Nombre          { get; set; }
+    public bool     LibreGluten     { get; set; }
+    public float    Importe         { get; set; }
+    public string   Descripcion     { get; set; }
+}
